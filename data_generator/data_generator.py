4f81f80b438847157071406d5ba69523afbdb033
Hamza1610 <muhammadhamza162003@gmail.com>
2024-04-29 15:39:45 -0700

    add data genreator class

---
+++ data_generator/data_generator.py
@@ -0,0 +1,14 @@
+
+class Data_generator:
+
+    def __init__(self):
+        pass
+
+    def generate(self, description, filetype, features):
+        pass
+
+    def download(self, filename):
+        pass
+
+    def preview(self, filepath, start, stop):
+        pass
