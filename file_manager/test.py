f1ab21e626ef09cf7394cdb9e6e620fa1e449e87
Hamza1610 <muhammadhamza162003@gmail.com>
2024-04-29 15:41:52 -0700

    created testing file

---
+++ file_manager/test.py
@@ -0,0 +1,8 @@
+from file import File_manager
+
+file = File_manager()
+
+print(file.create('test.json'))
+# print(file.update('test.txt', 'Great change', 23))
+# print(file.gettinfo('test.txt', ['size', 'format', 'preview']))
+# print(file.delete('test.txt'))
