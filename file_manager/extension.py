7daa830ff175a0f195ca997926c3fa6517003baf
Hamza1610 <muhammadhamza162003@gmail.com>
2024-05-02 07:17:18 -0700

    add file extensionn checker

--- file_manager/extension.py
+++ file_manager/extension.py
@@ -1,6 +1,15 @@
 
 def isformat(filename):
-    if filename and filename in ['.json', '.csv','.txt','.xlsx']:
-        return True
+    ext = ['.json', '.csv','.txt','.xlsx']
+    if filename and type(filename) == str:
+        for item in ext:
+            if filename.endswith(item):
+                return True
+            else:
+                continue
+    elif filename == '':
+        print('Filename is empty')
+        return False    
     else:
+        print('Filename is not a string')
         return False
